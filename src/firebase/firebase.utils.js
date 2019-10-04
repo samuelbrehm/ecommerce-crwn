496a5e8dffdacf00add6494b5980e75ffc7dfb9e
Samuel Brehm <samuelbrehm@gmail.com>
2019-10-04 16:09:42 -0300

    Fix file firebase. Update header component including option signin and form signin component for button google auth styling

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -20,6 +20,6 @@ export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = auth.signInWithPopup(provider);
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
