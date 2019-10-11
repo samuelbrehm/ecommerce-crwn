c67ebbaf00f1180a719198c106585a06ca877a3a
Samuel Brehm <samuelbrehm@gmail.com>
2019-10-11 16:02:30 -0300

    Built stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.compoent.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_y0Kikttqx97rgxe4tJx9gDy600yM554sA3';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
