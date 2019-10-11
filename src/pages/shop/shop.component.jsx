a46df017173299340445fed1ec5f7975e4dbe288
Samuel Brehm <samuelbrehm@gmail.com>
2019-10-10 21:28:53 -0300

    Created reducer and selectors directory. Updating shop.data.js, reducer and selectors shop. Created collections overview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,11 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.compoent';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollections }) => (
-          <CollectionPreview key={id} {...otherCollections} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = () => (
+  <div className="shop-page">
+    <CollectionsOverview />
+  </div>
+);
 
 export default ShopPage;
